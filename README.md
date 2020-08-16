97f330e4dff75d9f2036c02765759ad51fdba637
sebaslh12 <sebaslh12@gmail.com>
2020-08-16 12:32:16 -0500

    Adding delete all button after a reminder has been added. Fixing class name. Updating readme.

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Calendar Challenge
 
-React calendar demo which shows the reminders for each day and the forecast for the city of the reminder. Reminders can be created, updated, and deleted. These are the restrictions for this demo:
+React calendar demo which shows the reminders for each day and the forecast for the city of the reminder. Reminders can be created, updated, and deleted. These are the restrictions and some considerations for this demo:
 - It doesn't have a backend so the data doesn't persist (be careful when you reload the page).
 - The days before today are disabled, it doesn't make sense to create a reminder for a day that has already passed.
+- There are 2 ways to delete a day's reminders, after you add reminder a new button will appear on the top left of the day, the other way is in the modal that shows all the day's reminders at the bottom it has a button to delete them all.
 - The cities are fixed because the weather API requires the city's latituted and longitude which is something that most people doesn't know.
 - Since I'm using the free version of the weather API, I'm unable to set forecast information that are further than 7 days from today.
 - The calendar supports every single month of the current year (After December it loops back to January 2020).
+
+## Completed Bonuses
+- [x] Expand the calendar to support more than the current month.
+- [x] Properly handle overflow when multiple reminders appear on the same date.
+- [x] Functionality to delete one or ALL the reminders for a specific day
 
 [You can check it out here](https://nostalgic-ride-c3ab0d.netlify.app/)
 
@@ -33,7 +39,7 @@ If you already have an account then run `npm run build` this command will create
 
 ## Authors
 
-* [**Sebastian Lozano Herrera**](https://github.com/sebaslh12) 
+* [**Sebastian Lozano Herrera**](https://github.com/sebaslh12)
 
 Contact information: sebaslh12@gmail.com.
 
--- src/components/Calendar/Day.js
+++ src/components/Calendar/Day.js
@@ -5,19 +5,25 @@ import dayjs from 'dayjs';
 import Reminder from '../Reminder';
 import RemindersModal from '../Reminder/RemindersModal';
 import { reminderSorter, getRemindersOfTheDay } from '../../Utils';
+import { deleteDayReminders } from '../../store/actions/reminders';
 
-const Day = ({ dayData, reminders }) => {
+const Day = ({ dayData, reminders, deleteDayReminders }) => {
 	const remindersOfThisDay = reminderSorter(getRemindersOfTheDay(dayData, reminders));
 	const isActive = (dayData.unix() >= dayjs().unix());
 	const [showModal, setShowModal] = useState(false);
 	const [showRemindersModal, setShowRemindersModal] = useState(false);
 	const toggleModal = () => setShowModal(!showModal);
 	const toggleRemindersModal = () => setShowRemindersModal(!showRemindersModal);
+	const deleteAllReminders = () => {
+		const hasConfirm = window.confirm("This action will delete all reminders of this day and cannot be undone");
+		if (hasConfirm) return deleteDayReminders({ date: dayData.date(), month: dayData.month() })
+	};
 
 	return (
 		<div className={`day${!isActive ? ' disabled' : ''}`}>
 			<h3>{dayData.date()}</h3>
 			<h3 className="day__add" onClick={toggleModal}>+</h3>
+			{!!remindersOfThisDay.length && <h3 className="day__remove" onClick={deleteAllReminders}>X</h3>}
 			<div className="reminders-container">
 				{remindersOfThisDay.slice(0, 2).map((reminder, index) => <Reminder key={index} reminder={reminder} />)}
 				{remindersOfThisDay.length > 2 &&
@@ -26,7 +32,7 @@ const Day = ({ dayData, reminders }) => {
 					</div>}
 			</div>
 			{showModal && <ReminderModal isOpen={showModal} onClose={toggleModal} dayData={dayData} />}
-			{showRemindersModal && <RemindersModal isOpen={showRemindersModal} onClose={toggleRemindersModal} reminders={remindersOfThisDay} day={dayData}/>}
+			{showRemindersModal && <RemindersModal isOpen={showRemindersModal} onClose={toggleRemindersModal} reminders={remindersOfThisDay} day={dayData} />}
 		</div>
 	);
 }
@@ -34,4 +40,8 @@ const mapStateToProps = state => ({
 	reminders: state.reminders,
 });
 
-export default connect(mapStateToProps)(Day);
+const mapDispatchToProps = dispatch => ({
+	deleteDayReminders: dayData => dispatch(deleteDayReminders(dayData))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Day);
